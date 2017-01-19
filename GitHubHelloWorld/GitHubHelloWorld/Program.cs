f435455b2daf73a68ac246172e797c6ab4f463c6
oskarbohm <oskarbohm@live.se>
2017-01-19 09:53:29 +0100

    Hello_World

---
+++ GitHubHelloWorld/GitHubHelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProgram myprogram = new MyProgram();
+
+            myprogram.run();
+        }
+    }
+}
